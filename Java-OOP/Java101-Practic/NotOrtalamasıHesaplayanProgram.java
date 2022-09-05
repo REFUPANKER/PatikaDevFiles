069232ba7e6feea06a82d47f394e7fbe1542bffb
REFUPANKER <68808212+REFUPANKER@users.noreply.github.com>
2022-09-05 13:22:45 +0300

    Create NotOrtalamasıHesaplayanProgram.java

---
+++ Java-OOP/Java101-Practic/NotOrtalamasıHesaplayanProgram.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc1=new Scanner(System.in);
+        Calc(new int[]{
+            readInt(sc1,"Matematik Notu:"),
+            readInt(sc1,"Fizik Notu:"),
+            readInt(sc1,"Kimya Notu:"),
+            readInt(sc1,"Turkce Notu:"),
+            readInt(sc1,"Tarih Notu:"),
+            readInt(sc1,"Muzik Notu:"),
+        });
+        sc1.close();
+    }
+    static void Calc(int notes[]){
+        int all=0;
+        for(int i=0;i<notes.length;i++)
+        {
+            all+=notes[i];
+        }
+        String calcResult=(all>=60)? "Gecti" : "Kaldi";
+        System.out.println("Sınıf Durumu :"+calcResult);
+         
+    }
+    static int readInt(Scanner sc,Object msg) {
+        System.out.print(msg);
+        return sc.nextInt();
+    }
+}
