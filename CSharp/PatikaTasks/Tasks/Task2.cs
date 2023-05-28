b5e326bd4a173b5556679b373541d20e89750303
REFUPANKER <68808212+REFUPANKER@users.noreply.github.com>
2023-05-28 14:53:23 +0300

    PatikaDevCSharpTasks
    All in one
    PatikaDev CSharp Tasks

---
+++ CSharp/PatikaTasks/Tasks/Task2.cs
@@ -0,0 +1,7 @@
+public class Task2 : FastCommands, TaskTheme
+{
+    public void Run()
+    {
+       cwl("Task2 running");
+    }
+}
---
+++ CSharp/PatikaTasks/Tasks/TaskTheme.cs
@@ -0,0 +1,4 @@
+public interface TaskTheme
+{
+    public void Run();
+}
